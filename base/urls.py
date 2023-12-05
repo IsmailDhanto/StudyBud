9ae9594f05a53366393a321c8e025800c2a1cca3
IsmailDhanto <dhanto05@gmail.com>
2023-12-05 15:16:03 +0300

    created new urls for user registration

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ from base import views
 urlpatterns = [
     path('login/',views.loginPage,name='login'),
     path('logout/',views.LogoutUser,name='logout'),
+    path('register/',views.registerUser,name='register'),
     path('',views.home,name='home'),
     path("rooms/<str:pk>/",views.room,name='rooms'),
     path('create-room/', views.createRoom, name='create-room'),
