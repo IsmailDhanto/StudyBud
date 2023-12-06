11f5a842ea1996c5b43a569d67472a5b15855200
IsmailDhanto <dhanto05@gmail.com>
2023-12-06 15:16:07 +0300

    some migrations and added delete-message function

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from .forms import RoomForm
-from .models import Room, Topic
+from .models import Message, Room, Topic
 
 # Create your views here.
 
@@ -85,7 +85,20 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body'),
+
+        )
+        room.participants.add(request.user)
+        return redirect('rooms',pk=room.id)
+
+
+    context = {'room':room,'room_messages':room_messages,'participants':participants}
 
 
     return render(request, 'base/room.html',context)
@@ -127,7 +140,14 @@ def deleteRoom(request,pk):
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':room})
 
-
-
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse('Youre not allowed here ')
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':message})
 
 
